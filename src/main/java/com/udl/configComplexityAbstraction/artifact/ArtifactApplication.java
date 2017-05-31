4a22252f168017c4c376342ed9fb747f1d086631
jmelich <melichpelegri@gmail.com>
2017-05-31 18:06:16 +0200

    Project Initialization

---
+++ src/main/java/com/udl/configComplexityAbstraction/artifact/ArtifactApplication.java
@@ -0,0 +1,12 @@
+package com.udl.configComplexityAbstraction.artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactApplication.class, args);
+	}
+}

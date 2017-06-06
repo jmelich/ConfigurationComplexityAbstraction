ffe02db8eb97cb823635673d5f45bda290b00e36
jmelich <melichpelegri@gmail.com>
2017-06-06 17:27:15 +0200

    adding controller to get Available Speeds

--- src/main/java/cat/udl/eps/switchconfiguration/controller/ApplyConfigurationController.java
+++ src/main/java/cat/udl/eps/switchconfiguration/controller/ApplyConfigurationController.java
@@ -1,11 +1,15 @@
 package cat.udl.eps.switchconfiguration.controller;
 
+import cat.udl.eps.switchconfiguration.domain.Connector;
+import cat.udl.eps.switchconfiguration.repository.ConnectorRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,12 +21,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ApplyConfigurationController {
     private final Logger logger = LoggerFactory.getLogger(ApplyConfigurationController.class);
 
-    @RequestMapping(value = "/configurar", method = RequestMethod.GET)
+
+    @Autowired private ConnectorRepository connectorRepository;
+
+    @RequestMapping(value = "/connectors/{id}/availableSpeed", method = RequestMethod.GET)
     @PreAuthorize("isAuthenticated()")
     /*public @ResponseBody UserDetails getCurrentUser() {
         return (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }*/
-    public @ResponseBody void getCurrentUser() {
-        logger.error("HA DEMANAT CONFIGURAR");
+    public @ResponseBody void getAvailableSpeeds(@PathVariable("id") Long id) {
+
+        logger.info("User Requested Available Speeds of Connector: "+String.valueOf(id));
+
+        Connector connector = connectorRepository.findOne(id);
+
+
+
     }
 }

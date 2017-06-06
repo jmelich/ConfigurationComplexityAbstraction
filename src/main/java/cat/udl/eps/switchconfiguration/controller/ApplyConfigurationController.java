db092c6432169c04ec49121fc6058578c2301689
jmelich <melichpelegri@gmail.com>
2017-06-06 17:59:11 +0200

    config for available speed

--- src/main/java/cat/udl/eps/switchconfiguration/controller/ApplyConfigurationController.java
+++ src/main/java/cat/udl/eps/switchconfiguration/controller/ApplyConfigurationController.java
@@ -1,10 +1,14 @@
 package cat.udl.eps.switchconfiguration.controller;
 
 import cat.udl.eps.switchconfiguration.domain.Connector;
+import cat.udl.eps.switchconfiguration.domain.Equipment;
 import cat.udl.eps.switchconfiguration.repository.ConnectorRepository;
+import cat.udl.eps.switchconfiguration.repository.EquipmentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by ubuntudesktop on 6/06/17.
@@ -34,8 +39,23 @@ public class ApplyConfigurationController {
         logger.info("User Requested Available Speeds of Connector: "+String.valueOf(id));
 
         Connector connector = connectorRepository.findOne(id);
+        Equipment equipment = connector.getIsInEquipment();
+
         int usedPortInEquipment = connector.getEquipmentPort();
-        Long equipmentIP = connector.getIsInEquipment().getId();
+        String equipmentIP = equipment.getIP();
+        String username = "";
+        String password = "";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String URL = String.format("https://%s/auth/?&username=%s&password=%s",equipmentIP,username,password);
+
+        ResponseEntity<String> response = restTemplate.getForEntity(URL, String.class);
+
+        if(response.getStatusCode() == HttpStatus.OK){
+            //do something
+        }
+
 
     }
 }

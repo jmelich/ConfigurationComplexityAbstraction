da8eb54232013543c1987b8890d319be3a69812a
jmelich <melichpelegri@gmail.com>
2017-06-06 14:15:45 +0200

    Added basic controller to check if works

---
+++ src/main/java/cat/udl/eps/switchconfiguration/controller/ApplyConfigurationController.java
@@ -0,0 +1,28 @@
+package cat.udl.eps.switchconfiguration.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by ubuntudesktop on 6/06/17.
+ */
+@Controller
+public class ApplyConfigurationController {
+    private final Logger logger = LoggerFactory.getLogger(ApplyConfigurationController.class);
+
+    @RequestMapping(value = "/configurar", method = RequestMethod.GET)
+    @PreAuthorize("isAuthenticated()")
+    /*public @ResponseBody UserDetails getCurrentUser() {
+        return (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }*/
+    public @ResponseBody void getCurrentUser() {
+        logger.error("HA DEMANAT CONFIGURAR");
+    }
+}

cbc8e8fe0d3d850d29b8ed4d0f14b977c868066b
Jordi Mèlich <jordicanon@gmail.com>
2017-06-21 18:03:05 +0200

    problems with character + so replaced for as pace

--- src/main/java/cat/udl/eps/switchconfiguration/controller/ApplyConfigurationController.java
+++ src/main/java/cat/udl/eps/switchconfiguration/controller/ApplyConfigurationController.java
@@ -3,17 +3,30 @@ package cat.udl.eps.switchconfiguration.controller;
 import cat.udl.eps.switchconfiguration.domain.Connector;
 import cat.udl.eps.switchconfiguration.domain.Equipment;
 import cat.udl.eps.switchconfiguration.repository.ConnectorRepository;
+import org.apache.http.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,12 +35,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.security.cert.CertificateException;
+import java.net.URI;
+import java.nio.charset.Charset;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by ubuntudesktop on 6/06/17.
@@ -67,7 +92,7 @@ public class ApplyConfigurationController {
         //restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
         //RestTemplate restTemplate = new org.springframework.web.client.RestTemplate(new HttpComponentsClientHttpRequestFactory());
 
-        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+        /*TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
 
         SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
                 .loadTrustMaterial(null, acceptingTrustStrategy)
@@ -86,15 +111,17 @@ public class ApplyConfigurationController {
 
         RestTemplate restTemplate = new RestTemplate(requestFactory);
 
-        String URL = String.format("https://%s/auth/?&username=%s&password=%s",equipmentIP,username,password);
+        String URL = String.format("http://%s/auth/?&username=%s&password=%s",equipmentIP,username,password);
         logger.info(URL);
 
         ResponseEntity<String> response = restTemplate.getForEntity(URL, String.class);
         logger.info(response.toString());
 
+        logger.info(response.getHeaders().get("Set-Cookie").get(0));
+
         if(response.getStatusCode() == HttpStatus.OK){
             //make call to get available speeds
-            URL = String.format("https://%s/cli/aos?&cmd=show+interfaces+1/1/%s",equipmentIP,usedPortInEquipment);
+            URL = String.format("http://%s/cli/aos?&cmd=show+interfaces+1/1/%s",equipmentIP,usedPortInEquipment);
             logger.info(URL);
             try{
                 response = restTemplate.getForEntity(URL, String.class);
@@ -106,9 +133,34 @@ public class ApplyConfigurationController {
             //return response to client
         }else{
             logger.error("Cannot locate equipment or username and password are wrong");
+        }*/
+
+        //first request
+
+
+
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<String> forEntity = template.getForEntity("http://192.168.1.1/auth/?&username=admin&password=switch", String.class);
+        String cookies = forEntity.getHeaders().get("Set-Cookie").get(0).split(";")[0];
+        logger.info(cookies);
+
+        if(forEntity.getStatusCode()== HttpStatus.OK){
+
+            RestTemplate restTemplate = new RestTemplate();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Cookie",cookies);
+            //headers.set("Accept", "application/json");
+            //headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+            //headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.set("Content-Type",MediaType.APPLICATION_JSON_VALUE);
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<String> response = restTemplate.exchange("http://192.168.1.1/cli/aos?cmd=show interfaces 1/1/1", HttpMethod.GET, entity, String.class);
+
+            logger.info(response.toString());
         }
-
-
     }
 
 

ffe41adfbcc89c262c06ae05a4c35137420fbf92
jmelich <melichpelegri@gmail.com>
2017-07-22 12:49:20 +0200

    finding Ignoring case for Floor

--- src/main/java/cat/udl/eps/switchconfiguration/repository/FloorRepository.java
+++ src/main/java/cat/udl/eps/switchconfiguration/repository/FloorRepository.java
@@ -1,5 +1,7 @@
 package cat.udl.eps.switchconfiguration.repository;
 
+import cat.udl.eps.switchconfiguration.domain.Building;
+import cat.udl.eps.switchconfiguration.domain.Campus;
 import cat.udl.eps.switchconfiguration.domain.Floor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +13,7 @@ import java.util.List;
 @RepositoryRestResource
 public interface FloorRepository extends PagingAndSortingRepository<Floor, Long> {
     List<Floor> findByDescription(@Param("description") String description);
-    List<Floor> findByTitleContaining(@Param("title") String title);
-    List<Floor> findByDescriptionContaining(@Param("description") String description);
+    List<Floor> findByTitleContainingIgnoreCase(@Param("title") String title);
+    List<Floor> findByDescriptionContainingIgnoreCase(@Param("description") String description);
+    List<Floor> findByTitleContainingIgnoreCaseAndIsInBuilding(@Param("title") String title, @Param("building") Building building);
 }
